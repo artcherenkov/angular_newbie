d5d4b63a6d2c2468b13b9240c6e6a1fb3210f5f9
Артем Черенков <art.cherenkov@gmail.com>
2020-11-05 23:53:19 +0700

    Подлючает сервис `Cart` к компоненту `Cart`, а также  создает view компонента `Cart`

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+// импортируем сервис `Cart`
+import { CartService } from '../cart.service';
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  items;
 
-  constructor() { }
+  // инжектнем сервис в сигнатуру конструктора
+  constructor(
+    private cartService: CartService
+  ) { }
 
   ngOnInit(): void {
+    // установим список продуктов в корзине
+    this.items = this.cartService.getItems();
   }
 
 }
